09fffc958cc16f457e8021c02c9b0f617d0bc1c4
codeBender31 <ibarraisrael949@yahoo.com>
2024-03-16 13:04:55 -0500

    Initial Commit

---
+++ FlashcardLab/FlashcardLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardLabApp.swift
+//  FlashcardLab
+//
+//  Created by Bender on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
